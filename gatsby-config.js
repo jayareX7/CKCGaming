08bfea6013b987c8f06c4369374ba32cf3d118fe
Admin <jayarex07@gmail.com>
2022-05-13 12:12:38 -0400

    updated node modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,154 @@
+/* eslint-disable prettier/prettier */
+const config = require("./config/config");
+const activeEnv =
+    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
 module.exports = {
+    pathPrefix: config.pathPrefix,
     siteMetadata: {
-        title: `HiStaff`,
-        description: `Make your Staff and Workspace Happy`,
-        author: `@mostafasoufi`,
+        title: config.title,
+        titleTemplate: config.titleTemplate,
+        description: config.description,
+        image: config.image,
+        siteLanguage: config.siteLanguage,
+        author: config.author,
+        mainUrl: config.siteUrl,
+        siteUrl:
+            activeEnv === "development"
+                ? config.localUrl
+                : `${config.siteUrl}${config.pathPrefix}`,
+        canonical: config.canonical,
+        twitterUsername: config.twitterUsername,
+        keywords: config.keywords,
+    },
+    flags: {
+        DEV_SSR: true,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/static/images`,
-            },
-        },
+        `gatsby-plugin-image`,
+        `gatsby-transformer-json`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: `gatsby-plugin-postcss`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#FF54AC`,
-                display: `minimal-ui`,
-                icon: `${__dirname}/static/images/histaff.png`, // This path is relative to the root of the site.
+                postCssPlugins: [
+                    require(`tailwindcss`)({ stage: 0 }),
+                    //require("tailwindcss-pseudo-elements"),
+                ],
             },
         },
-        `gatsby-plugin-sass`,
         {
-            resolve: "gatsby-omni-font-loader",
+            resolve: "gatsby-plugin-purgecss",
             options: {
-                enableListener: true,
-                preconnect: ["https://fonts.gstatic.com"],
-
-                web: [
+                printRejected: false,
+                develop: false,
+                tailwind: true,
+                // purgeCSSOptions: {
+                //     safelist: [],
+                // },
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
                     {
-                        file: "https://fonts.googleapis.com/css2?family=PT+Serif&display=swap",
-                        name: "PT Serif",
-                    },
-                ],
-                custom: [
-                    {
-                        name: "Inter",
-                        file: "/fonts/fonts.css",
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1920,
+                        },
                     },
                 ],
             },
         },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "data",
+                path: `${__dirname}/src/data/`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-breadcrumb`,
+            options: {
+                useAutoGen: true,
+                autoGenHomeLabel: `Home`,
+                exclude: [`/dev-404-page`, `/404`, `/404.html`],
+                useClassNames: true,
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: `${__dirname}/src/assets/images/`,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-svgr-loader",
+            options: {
+                rule: {
+                    include: /\.svg$/,
+                },
+            },
+        },
+
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: config.title,
+                short_name: config.shortName,
+                theme_color: config.themeColor,
+                background_color: config.backgroundColor,
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icon: config.favicon,
+                icons: [
+                    {
+                        src: "/icons/icon-72x72.png",
+                        sizes: "72x72",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-96x96.png",
+                        sizes: "96x96",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-128x128.png",
+                        sizes: "128x128",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-144x144.png",
+                        sizes: "144x144",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-152x152.png",
+                        sizes: "152x152",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        },
     ],
 };
