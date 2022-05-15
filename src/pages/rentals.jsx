76c34351c4ee9124d07b3fcd65148e2b867aa661
Admin <jayarex07@gmail.com>
2022-05-15 13:35:26 -0400

    added rentals page

--- src/pages/rentals.jsx
+++ src/pages/rentals.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import SEO from "@components/seo";
 import Layout from "@layout";
+import { graphql } from "gatsby";
 import { normalizedData } from "@utils/functions";
+import PageBreadcrumb from "../components/pagebreadcrumb";
 
-const RentalsPage = ({ data }) => {
+const RentalsPage = ({ data, location, pageContext }) => {
     const globalContent = normalizedData(data?.allGeneral?.nodes || []);
 
     return (
@@ -13,9 +16,58 @@ const RentalsPage = ({ data }) => {
                 ...globalContent["footer"],
             }}
         >
-            <SEO title="Home" pathname="/" />
+            <SEO title="Rentals Page" pathname="/" />
+            <PageBreadcrumb
+                pageContext={pageContext}
+                location={location}
+                title="Rentals"
+            />
         </Layout>
     );
 };
+
+RentalsPage.propTypes = {
+    location: PropTypes.object,
+    pageContext: PropTypes.object,
+    data: PropTypes.shape({
+        allGeneral: PropTypes.shape({
+            nodes: PropTypes.arrayOf(PropTypes.shape({})),
+        }),
+        page: PropTypes.shape({
+            content: PropTypes.arrayOf(PropTypes.shape({})),
+        }),
+    }),
+};
+
+export const query = graphql`
+    query RentalsPageQuery {
+        allGeneral {
+            nodes {
+                section
+                id
+                menu {
+                    ...Menu
+                }
+                footer {
+                    ...Footer
+                }
+                footer2 {
+                    ...Footer2
+                }
+                footer3 {
+                    ...Footer3
+                }
+                footer4 {
+                    ...Footer4
+                }
+            }
+        }
+        page(title: { eq: "FAQPage" }, pageType: { eq: innerpage }) {
+            content {
+                ...PageContentAll
+            }
+        }
+    }
+`;
 
 export default RentalsPage;
